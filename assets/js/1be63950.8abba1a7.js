ae321040a05825c5c1137163938eb4da13c01394
pramod Gogadare <pramod.gogadare@waynautic.com>
2026-04-16 18:59:18 +0530

    Deploy website - based on 0a0916989bd3b34d7dc2453bab58d6c55794245f

---
+++ assets/js/1be63950.8abba1a7.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkhelp_site=globalThis.webpackChunkhelp_site||[]).push([[5112],{3446(e){e.exports=JSON.parse('{"metadata":{"permalink":"/exgenix-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.72f8dbda.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkhelp_site=globalThis.webpackChunkhelp_site||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/exgenix-docs/blog","blogTitle":"Blog","authorsListPath":"/exgenix-docs/blog/authors"}')}}]);
